79377720c8b3fe09dc8b6c890b2bf706972a32c8
Davi Eduardo <davieduardocostadf@outlook.com>
2025-06-29 17:34:57 -0300

    update on some paths

--- apply.sh
+++ apply.sh
@@ -2,8 +2,8 @@
 
 CONFIG_DIR="$HOME/.config/fastfetch"
 
-HOME_DIR="$HOME/configs/fasfetch"
-HOME_FILE="$HOME/configs/fastfetch/minimal.jsonc"
+HOME_DIR="$HOME/configs/fastfetch-my-conf"
+HOME_FILE="$HOME/configs/fastfetch-my-conf/minimal.jsonc"
 
 # Verifica se o fastfetch está instalado
 if command -v fastfetch >/dev/null 2>&1; then
@@ -25,7 +25,7 @@ echo "Criando diretório de configuração, se não existir..."
 mkdir -p "$CONFIG_DIR"
 
 echo "Copiando arquivo de configuração para $CONFIG_DIR ..."
-cp "./$HOME_FILE" "$CONFIG_DIR"
+cp "$HOME_FILE" "$CONFIG_DIR"
 
 echo "Configuração aplicada! Você pode executar:"
 echo "  fastfetch --load-config minimal"
